bf912510e4de98ea23f4407206b1bc16b800cd95
Emi55 <emilieahfock@gmail.com>
2020-12-01 22:03:16 -0500

    first commit

---
+++ src/components/AddHeroBtn.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "add-hero-btn",
+    props:{
+        text:String,
+    }
+};
+</script>
+
+<template>
+    <a class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-light-purple" 
+    href="#0" 
+    @click="$emit('add-hero', true)">
+    {{ text }}
+    </a>
+</template>
